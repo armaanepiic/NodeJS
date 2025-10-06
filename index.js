a225995cf8ef631836d863a724b03f9307643c7c
armaanepiic <armaanepiicfb17@gmail.com>
2025-10-07 00:28:59 +0600

    - module object, - module.exports, - require method

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log(window); // window is not defined
+// setTimeout(()=>{
+//     console.log("test");
+// },2000);
+
+// console.log(global);
+// console.log(__dirname); // /home/armaan/projects/NodeJS
+// console.log(__filename); // /home/armaan/projects/NodeJS/index.js
+
+
+const people = require('./people');
+console.log(people.people);
+console.log(people.a);
+console.log(people.test);
