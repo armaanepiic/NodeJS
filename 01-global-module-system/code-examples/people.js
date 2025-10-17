3109440a454bc1f5314fe95c671fb2b94bdf4db6
armaanepiic <armaanepiicfb17@gmail.com>
2025-10-17 17:52:48 +0600

    module system refresher

--- 01-global-module-system/code-examples/people.js
+++
@@ -1,9 +0,0 @@
-const people = ["sakib", "tamim", "rubel"];
-var a = 6;
-const test = () => {
-  console.log("I am test function");
-};
-console.log(module);
-module.exports = { people, a, test };
-console.log(module);
-// returns the module.exports;
---
+++ 01-global-module-system/people.js
@@ -0,0 +1,7 @@
+const people = ["sakib", "tamim", "rubel"];
+var a = 70;
+function test () {
+  console.log("I am a test function!");
+}
+module.exports = {people, a, test};
+// console.log(module);
