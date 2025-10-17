12b9d079d5cb4b998f09d489733bf807384773ec
armaanepiic <armaanepiicfb17@gmail.com>
2025-10-17 19:17:11 +0600

    os-module

--- 02-core-modules/os-module.js
+++ 02-core-modules/os-module.js
@@ -1,5 +1,33 @@
 const os = require('os');
-console.log(os.platform());
-console.log(os.homedir());
-console.log(os.freemem());
-console.log(os.cpus());
+let os_value;
+
+os_value = os.platform(); // linux
+os_value = os.homedir();
+os_value = os.freemem();
+os_value = os.cpus();
+
+console.log(os_value); 
+
+// os.EOL
+// os.availableParallelism()
+// os.arch()
+// os.constants
+// os.cpus()
+// os.devNull
+// os.endianness()
+// os.freemem()
+// os.getPriority([pid])
+// os.homedir()
+// os.hostname()
+// os.loadavg()
+// os.machine()
+// os.networkInterfaces()
+// os.platform()
+// os.release()
+// os.setPriority([pid, ]priority)
+// os.tmpdir()
+// os.totalmem()
+// os.type()
+// os.uptime()
+// os.userInfo([options])
+// os.version()
