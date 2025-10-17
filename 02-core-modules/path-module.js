b71c882a7aa205e847cf0bf3a6a64be698ad348d
armaanepiic <armaanepiicfb17@gmail.com>
2025-10-17 19:17:21 +0600

    path module

--- 02-core-modules/path-module.js
+++ 02-core-modules/path-module.js
@@ -1,22 +1,33 @@
 const path = require('path');
-const myPath = '/home/armaan/projects/NodeJS/index.js';
-console.log(path.basename(myPath));
-console.log(path.dirname(myPath));
-console.log(path.extname(myPath));
-console.log(path.parse(myPath));
-console.log(path.delimiter);
-console.log(path.format(path.parse(myPath)));
-console.log(path.normalize(myPath));
+const myPath = '/home/armaan/projects/NodeJS/02-core-modules/path-module.js';
 
-path.format(pathObject)
-path.matchesGlob(path, pattern)
-path.isAbsolute(path)
-path.join([...paths])
-path.normalize(path)
-path.parse(path)
-path.posix
-path.relative(from, to)
-path.resolve([...paths])
-path.sep
-path.toNamespacedPath(path)
-path.win32
+let path_value;
+path_value = path.basename(myPath);
+path_value = path.dirname(myPath);
+path_value = path.extname(myPath);
+path_value = path.parse(myPath);
+path_value = path.delimiter;
+path_value = path.format(path.parse(myPath));
+
+
+
+
+
+console.log(path_value);
+
+
+// console.log(path.format(path.parse(myPath)));
+// console.log(path.normalize(myPath));
+
+// path.format(pathObject)
+// path.matchesGlob(path, pattern)
+// path.isAbsolute(path)
+// path.join([...paths])
+// path.normalize(path)
+// path.parse(path)
+// path.posix
+// path.relative(from, to)
+// path.resolve([...paths])
+// path.sep
+// path.toNamespacedPath(path)
+// path.win32;
