513eacaf67b02a7c65943838c3aea7da652196d7
arman <armaanepiic@gmail.com>
2026-07-20 16:51:55 +0600

    feat:custom status code

--- Lesson-6/server.js
+++ Lesson-6/server.js
@@ -2,9 +2,9 @@ const http = require("http");
 const port = 3000;
 const hostname = "127.0.0.1";
 const myServer = http.createServer((req, res) => {
-    res.writeHead(202, {'Content-Type':'text/html'});
-    res.write("<h1>hello</h1>");
-    res.end();
+  res.writeHead(202, { "Content-Type": "text/html" });
+  res.write("<h1>hello</h1>");
+  res.end();
 });
 myServer.listen(port, hostname, () => {
   console.log(`Server is running at ${hostname}:${port}`);
