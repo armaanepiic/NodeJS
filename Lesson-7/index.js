27cee4c73b59ed269f041b1bfd4e1f050c836586
arman <armaanepiic@gmail.com>
2026-07-20 18:34:19 +0600

    feat: nodemon and routing basics

---
+++ Lesson-7/index.js
@@ -0,0 +1,9 @@
+// const getRandomFruitsName = require('random-fruits-name')
+// console.log(getRandomFruitsName());
+
+
+const moviesNames = require('movies-names');
+// moviesNames.all # returns all movies
+// moviesNames.random() # returns a random movie name
+console.log(moviesNames.all)
+console.log(moviesNames.random())
